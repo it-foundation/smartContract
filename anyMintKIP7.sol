4553662aa4278ddbbc3aeac2c66e0f1dc94d2c3b
bringitbackalive <107536317+bringitbackalive@users.noreply.github.com>
2022-08-30 15:37:43 +0800

    Update anyMintKIP7.sol

--- anyMintKIP7.sol
+++ anyMintKIP7.sol
@@ -3,6 +3,14 @@ pragma solidity ^0.8.0;
 
 import "https://github.com/klaytn/klaytn-contracts/blob/master/contracts/KIP/token/KIP7/KIP7.sol";
 
+// if theres is error when compiling go to the file with error and replace the import github with the 
+// actual link of the file from klaytn repo (get reference from the above link its the same repo above)
+
+
+//deployed smart contract address is 0xE60e0C77A7Aa70Aaf9B80bae0BfadCb4A87693aD
+
+
+
 contract KIP7Token is KIP7 {
 
    
