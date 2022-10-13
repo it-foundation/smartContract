6629adfe8d992886217999c028e1035288a5830e
bringitbackalive <107536317+bringitbackalive@users.noreply.github.com>
2022-10-13 16:36:15 +0800

    final

--- erc20.sol
+++ erc20.sol
@@ -2,19 +2,15 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, Ownable {
+contract ITToken is ERC20 {
     constructor() ERC20("MyToken", "MTK") {}
 
-    function mint(uint256 amount) public onlyOwner {
+    function mint(uint256 amount) public {
         _mint(msg.sender, amount);
     }
     
-    function transfer(address to, uint256 amount) public override returns(bool) {
-        _transfer(msg.sender, to, amount);
-        return true;
+    function transfer(address from, address to, uint256 amount) public {
+        _transfer(from, to, amount);
     }
-    
-
 }
