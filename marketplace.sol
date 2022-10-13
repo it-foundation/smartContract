9938ec76997dd5ac6f8b0235dc7f1eb05a64587f
bringitbackalive <107536317+bringitbackalive@users.noreply.github.com>
2022-10-13 17:47:32 +0800

    final

--- marketplace.sol
+++ marketplace.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
+import "./ITToken.sol";
 
 //The structure to store info about a listed token
 struct ListedToken {
@@ -20,6 +21,7 @@ contract NFTMarketplace is ERC1155, Ownable, ERC1155Holder {
     
     string public name;
     string public symbol;
+    ITToken public _itt;
 
     //the event emitted when a token is successfully listed
     event TokenListedSuccess (
@@ -35,9 +37,10 @@ contract NFTMarketplace is ERC1155, Ownable, ERC1155Holder {
     // tokenId => uri
     mapping(uint => string) public tokenURI;
 
-    constructor() ERC1155("") {
+    constructor(ITToken itt) ERC1155("") {
         name = "ShopIt";
-        symbol = "IT";      
+        symbol = "IT";
+        _itt = itt;
     }
 
     function supportsInterface(bytes4 interfaceId)
@@ -48,11 +51,6 @@ contract NFTMarketplace is ERC1155, Ownable, ERC1155Holder {
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-
-    function getLatestIdToListedToken() public view returns (ListedToken memory) {
-        uint256 currentTokenId = _tokenIds.current();
-        return listedTokens[currentTokenId];
     }
 
     function getListedTokenForId(uint256 tokenId) public view returns (ListedToken memory) {
@@ -74,6 +72,10 @@ contract NFTMarketplace is ERC1155, Ownable, ERC1155Holder {
         tokenURI[tokenId] = _uri;
         emit URI(_uri, tokenId);
 
+
+
+
+
         listedTokens[tokenId] = ListedToken(
             payable(msg.sender),
             price,
@@ -83,8 +85,59 @@ contract NFTMarketplace is ERC1155, Ownable, ERC1155Holder {
         return tokenId;
     }
     
+
+
+    function getMyNFTs() public view returns (ListedToken[] memory) {
+        uint totalItemCount = _tokenIds.current();
+        uint itemCount = 0;
+        uint currentIndex = 0;
+        uint currentId;
+        //Important to get a count of all the NFTs that belong to the user before we can make an array for them
+        for(uint i=0; i < totalItemCount; i++)
+        {
+            if(listedTokens[i+1].seller == msg.sender){
+                itemCount += 1;
+            }
+        }
+
+                //Once you have the count of relevant NFTs, create an array then store all the NFTs in it
+        ListedToken[] memory items = new ListedToken[](itemCount);
+        for(uint i=0; i < totalItemCount; i++) {
+            if(listedTokens[i+1].seller == msg.sender) {
+                currentId = i+1;
+                ListedToken storage currentItem = listedTokens[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    function getAllNFTs() public view returns (ListedToken[] memory) {
+        uint nftCount = _tokenIds.current();
+        ListedToken[] memory tokens = new ListedToken[](nftCount);
+        uint currentIndex = 0;
+        uint currentId;
+        //at the moment currentlyListed is true for all, if it becomes false in the future we will 
+        //filter out currentlyListed == false over here
+        for(uint i=0;i<nftCount;i++)
+        {
+            currentId = i + 1;
+            ListedToken storage currentItem = listedTokens[currentId];
+            tokens[currentIndex] = currentItem;
+            currentIndex += 1;
+        }
+        //the array 'tokens' has the list of all NFTs in the marketplace
+        return tokens;
+    }
+
+
+
     function buy(uint256 tokenId, uint256 amount) public payable {
         this.safeTransferFrom(address(this), msg.sender, tokenId, amount, "");
+        ListedToken memory listedToken = listedTokens[tokenId];
+        _itt.transfer(msg.sender, listedToken.seller, listedToken.price);
         
         if(balanceOf(address(this), tokenId) == 0) {
             delete listedTokens[tokenId];
